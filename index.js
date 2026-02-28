c52efaede30f64a0f87ed83b75fb6c823ca63d31
WaseurGIT <waseurrahman31@gmail.com>
2026-02-28 08:31:24 +0600

    deploying in render

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const jwt = require("jsonwebtoken");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://tech-blogs-rho.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
