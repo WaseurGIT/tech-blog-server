fb19cca1322c2570099ecd8b4d5a8aa3d562c1b6
WaseurGIT <waseurrahman31@gmail.com>
2026-02-28 14:43:52 +0600

    deploying

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://blogify-swart-omega.vercel.app",
+      "https://tech-blogs-theta.vercel.app",
     ],
     credentials: true,
   }),
